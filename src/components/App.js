2b037e15bfa953267aab265799743f098f5decd3
Tereshka <tereshka07@gmail.com>
2019-11-15 00:50:52 +0900

    add empty files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Header from './Header';
 
 export default class App extends React.Component {
+
+  state = {
+    currentStep: 0,
+
+  }
+
   render() {
     return (
       <div className="form-container card">
+        <Header step={this.currentStep}/>
         <form className="form card-body" />
       </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default Headers = (props) => {
+    return (
+        <div>
+            {}
+        </div>
+    );
+}
